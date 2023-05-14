fa57798198d0b1a00a5d3a854374ec03baecc774
21Enzo17 <enzo.meneghini@fi.unju.edu.ar>
2023-05-14 01:07:16 -0300

    Testeando cualquier cosa

---
+++ TP2_Grupo11/src/main/java/ar/edu/unju/fi/controller/NuevoProductoController.java
@@ -0,0 +1,19 @@
+package ar.edu.unju.fi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import ar.edu.unju.fi.model.Producto;
+
+
+@Controller
+public class NuevoProductoController {
+    @GetMapping("/nuevo_producto")
+    public String getNuevoProductoPage(){
+        return "nuevo_producto";
+    }
+
+
+}
+
---
+++ TP2_Grupo11/src/main/java/ar/edu/unju/fi/model/Producto.java
@@ -0,0 +1,68 @@
+package ar.edu.unju.fi.model;
+
+public class Producto {
+    private String nombre;
+    private int cod;
+    private float precio;
+    private String categoria;
+    private int descuento;
+
+
+    public Producto() {
+    }
+
+    public Producto(String nombre, int cod, float precio, String categoria, int descuento) {
+        this.nombre = nombre;
+        this.cod = cod;
+        this.precio = precio;
+        this.categoria = categoria;
+        this.descuento = descuento;
+    }
+    
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCod() {
+        return this.cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    public float getPrecio() {
+        return this.precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public String getCategoria() {
+        return this.categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getDescuento() {
+        return this.descuento;
+    }
+
+    public void setDescuento(int descuento) {
+        this.descuento = descuento;
+    }
+
+    public float calcularDescuento () {
+        return this.precio - (this.precio * this.descuento)/100;
+    }
+    
+
+}
