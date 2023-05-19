9a5b0fb52bdc8bacb19c6c3aeb0e0d3db363bb24
Darío Vidal <96150592+DarioRv@users.noreply.github.com>
2023-05-19 15:33:01 -0300

    Agregar clase Sucursal, ListaSucursal y visualización de datos

--- TP2_Grupo11/src/main/java/ar/edu/unju/fi/controller/SucursalesController.java
+++ TP2_Grupo11/src/main/java/ar/edu/unju/fi/controller/SucursalesController.java
@@ -1,13 +1,19 @@
 package ar.edu.unju.fi.controller;
 
+import ar.edu.unju.fi.Listas.ListaSucursal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/sucursales")
 public class SucursalesController {
-    
-    @GetMapping("/sucursales")
-    public String getSucursales(){
+    ListaSucursal listaSucursales = new ListaSucursal();
+
+    @GetMapping("/listado")
+    public String getSucursales(Model model){
+        model.addAttribute("listaSucursales", listaSucursales.getListaSucursales());
         return "sucursales";
     }
 }
