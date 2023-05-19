683fc8a6e60067dade699dd7d8cf92c9b0371d53
Darío Vidal <96150592+DarioRv@users.noreply.github.com>
2023-05-19 16:09:28 -0300

    Añadir funcionalidad de agregar sucursales

--- TP2_Grupo11/src/main/java/ar/edu/unju/fi/controller/SucursalesController.java
+++ TP2_Grupo11/src/main/java/ar/edu/unju/fi/controller/SucursalesController.java
@@ -1,10 +1,14 @@
 package ar.edu.unju.fi.controller;
 
 import ar.edu.unju.fi.Listas.ListaSucursal;
+import ar.edu.unju.fi.model.Sucursal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/sucursales")
@@ -16,4 +20,21 @@ public class SucursalesController {
         model.addAttribute("listaSucursales", listaSucursales.getListaSucursales());
         return "sucursales";
     }
+
+    @GetMapping("/nueva-sucursal")
+    public String getNuevaSucursalPage(Model model){
+        Sucursal formSucursal = new Sucursal();
+        model.addAttribute("formSucursal", formSucursal);
+        return "nueva-sucursal";
+    }
+
+    @PostMapping("/nueva-sucursal")
+    public ModelAndView crearSucursal(Sucursal formSucursal){
+        ModelAndView modelView = new ModelAndView("sucursales");
+        listaSucursales.getListaSucursales().add(formSucursal);
+        modelView.addObject("listaSucursales", listaSucursales.getListaSucursales());
+        return modelView;
+    }
+
+    
 }
