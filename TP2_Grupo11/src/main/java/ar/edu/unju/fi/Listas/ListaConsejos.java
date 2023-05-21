1e5680fa785b03aa9bb072d5a6d87ee563ca3cb9
Ezequiel <ezequielmamani7>
2023-05-21 19:50:30 -0300

    Se añadió la documentación al controller de consejos

--- TP2_Grupo11/src/main/java/ar/edu/unju/fi/Listas/ListaConsejos.java
+++ TP2_Grupo11/src/main/java/ar/edu/unju/fi/Listas/ListaConsejos.java
@@ -17,6 +17,7 @@ public class ListaConsejos {
 	
 	public ListaConsejos() {
 		listaConsejos = new ArrayList <Consejo>();
+		//Consejos precargados
 		listaConsejos.add(new Consejo("Al cuidar de tu mascota puedes mejorar tu salud física y mental","¿Sabías que la hormona Oxitocina, que se secreta cuando el cuerpo experimenta placer, también se secreta cuando una mascota y su dueño experimentan interacciones positivas entre si?\r\n. Entre más fuerte sea el vínculo entre la persona y su mascota, mayor es la posibilidad de que la persona secrete esta hormona, creando estados de felicidad y también de salud.\r\n Se encuentra comprobado que al tener una estrecha relación con tu mascota e interactuar con esta regularmente, aparecen una serie de beneficios en tu vida. Estudios demuestran que las personas que cuidan de sus mascotas también podrían ser más saludables, a razón que al sacarlos a caminar o correr y también jugar activamente con ellos se hace ejercicio teniendo un impacto positivo en su cuerpo y mente. En particular se han demostrado beneficios de salud al disminuir la tasa de problemas cardiovasculares."));
 	}
 
@@ -24,7 +25,7 @@ public class ListaConsejos {
 	public String toString() {
 		return "ListaConsejos [listaConsejos=" + listaConsejos + "]";
 	}
-
+	
 	public List<Consejo> getListaConsejos() {
 		return listaConsejos;
 	}
