679935dc5a1052b623f51e2388c082afa90686bf
21Enzo17 <enzo.meneghini@fi.unju.edu.ar>
2023-06-04 23:13:17 -0300

    fixeo validacion

--- TP2_Grupo11/src/main/java/ar/edu/unju/fi/model/Consejo.java
+++ TP2_Grupo11/src/main/java/ar/edu/unju/fi/model/Consejo.java
@@ -7,10 +7,10 @@ import jakarta.validation.constraints.*;
 @Component
 public class Consejo {
 	@NotBlank(message="El titulo no puede estar vacío")
-	@Size(min=5,max=25,message = "El nombre debe ser mayor a 5 caracteres")
+	@Size(min=5,max=25,message = "El titulo debe ser mayor a 5 caracteres y menor a 25 caracteres")
 	private String titulo;
 	@NotBlank(message="La descripción no puede estar vacía")
-	@Size(min=5,max=550,message = "El nombre debe ser mayor a 5 caracteres")
+	@Size(min=5,max=550,message = "La descripción debe ser mayor a 5 caracteres y menor a 500 caracteres")
 	private String descripcion;
 	@NotNull(message="El valor de ID no puede estar vacio")
 	@Positive(message="El ID debe ser un numero igual a 1 o mayor")
