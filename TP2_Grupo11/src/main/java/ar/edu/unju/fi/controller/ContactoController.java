83ea24707db282f5a7e0681b6662b8711f8199ed
Ezequiel <ezequielmamani7>
2023-06-22 17:09:36 -0300

    Se arreglaron errores del repository de contacto

--- TP2_Grupo11/src/main/java/ar/edu/unju/fi/controller/ContactoController.java
+++ TP2_Grupo11/src/main/java/ar/edu/unju/fi/controller/ContactoController.java
@@ -1,30 +1,38 @@
 package ar.edu.unju.fi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import ar.edu.unju.fi.entity.Contacto;
+import ar.edu.unju.fi.service.IContactoService;
+import jakarta.validation.Valid;
 
 @Controller
 public class ContactoController {
+	@Autowired
+	IContactoService contactoService;
     @GetMapping("/contactos")
-    public String getContactos(){
+    public String getContactos(Model model){
+    	model.addAttribute("formContactos", new Contacto());
         return "contactos";
     }
     
-    @PostMapping("/formContactos")
-    public String getNuevoContacto(){
-    	return "contactos";
-    	}
-    
-    public ModelAndView contactos(@Valid @ModelAttribute("formContactos") Contacto formContacto, BindingResult result){
+    @PostMapping("/formContacto")
+    public ModelAndView contacto(@Valid @ModelAttribute("formContactos") Contacto formContacto, BindingResult result){
         ModelAndView modelView;
         if (result.hasErrors()){
             modelView = new ModelAndView("contactos");
         }
         else{
-            contactoService(formContacto);
+            contactoService.crearContacto(formContacto);
             modelView = new ModelAndView("contactos");
-            contactos.addObject("mensaje",true);
+            modelView.addObject("alerta",true);
         }
         return modelView;
     }
--- TP2_Grupo11/src/main/java/ar/edu/unju/fi/service/imp/ContactoSeviceImp.java
+++ TP2_Grupo11/src/main/java/ar/edu/unju/fi/service/imp/ContactoSeviceImp.java
@@ -1,10 +1,12 @@
 package ar.edu.unju.fi.service.imp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import ar.edu.unju.fi.entity.Contacto;
 import ar.edu.unju.fi.repository.ContactoRepository;
 import ar.edu.unju.fi.service.IContactoService;
+@Service
 
 public class ContactoSeviceImp implements IContactoService {
 
